edbf2be71ceb080b641d7cb54b46a5781017d15b
shivangibodh <47949043+shivangibodh@users.noreply.github.com>
2020-07-20 17:26:49 +0530

    Create JavaApp.java

---
+++ JavaApp.java
@@ -0,0 +1,40 @@
+class JavaApp{
+
+ 
+ 
+
+ 
+ 
+
+ 
+
+public static void main(String args[]){
+
+ 
+ 
+
+ 
+ 
+
+ 
+
+System.out.println("Hello Java");
+
+ 
+ 
+
+ 
+ 
+
+ 
+}
+
+ 
+ 
+
+ 
+
+ 
+
+ 
+}
